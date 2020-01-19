7fc9516b22c3613a0fcbeb435dac629eaf75ceab
emicsto <emicsto@gmail.com>
2020-01-19 21:06:21 +0100

    Add basic security configuration

---
+++ src/main/java/com/emicsto/flashcards/security/WebSecurityConfig.java
@@ -0,0 +1,22 @@
+package com.emicsto.flashcards.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors();
+        http.authorizeRequests()
+                .antMatchers("/api/**").permitAll()
+                .anyRequest().permitAll();
+
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+}
