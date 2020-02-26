f4a5f9e35e96af27646e065cc65c67c90f2155e4
emicsto <emicsto@gmail.com>
2020-02-26 18:08:05 +0100

    Add config for integration tests

---
+++ src/test/java/me/emicsto/flashcards/utils/WithMockCustomUserSecurityContextFactory.java
@@ -0,0 +1,24 @@
+package me.emicsto.flashcards.utils;
+
+import me.emicsto.flashcards.user.User;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.test.context.support.WithSecurityContextFactory;
+
+public class WithMockCustomUserSecurityContextFactory
+        implements WithSecurityContextFactory<WithMockCustomUser> {
+    @Override
+    public SecurityContext createSecurityContext(WithMockCustomUser customUser) {
+        SecurityContext context = SecurityContextHolder.createEmptyContext();
+
+        User principal = new User();
+        principal.setName("name");
+        principal.setId(1L);
+        Authentication auth =
+                new UsernamePasswordAuthenticationToken(principal, "password", principal.getAuthorities());
+        context.setAuthentication(auth);
+        return context;
+    }
+}
