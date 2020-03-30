18f7db13b52742e375c925c4ef99c903d6c5747a
emicsto <emicsto@gmail.com>
2020-03-30 18:16:08 +0200

    Enable pre-post annotations

--- src/main/java/me/emicsto/flashcards/security/WebSecurityConfig.java
+++ src/main/java/me/emicsto/flashcards/security/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package me.emicsto.flashcards.security;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -11,6 +12,7 @@ import org.springframework.security.web.authentication.www.BasicAuthenticationFi
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
+@EnableGlobalMethodSecurity(prePostEnabled=true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     private final JWTAuthorizationFilter jwtAuthorizationFilter;
 
